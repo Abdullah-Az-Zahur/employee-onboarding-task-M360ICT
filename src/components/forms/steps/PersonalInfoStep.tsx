e4951533d003a7eea99106d83b7754cbe663cf0f
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2025-08-17 23:21:46 +0600

    add : personal info step

--- src/components/forms/steps/PersonalInfoStep.tsx
+++ src/components/forms/steps/PersonalInfoStep.tsx
@@ -21,6 +21,66 @@ export default function PersonalInfoStep() {
           </FormItem>
         )}
       />
+
+      <FormField
+        name="personalInfo.email"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Email</FormLabel>
+            <FormControl>
+              <Input placeholder="Please Enter Your Email" {...field} />
+            </FormControl>
+          </FormItem>
+        )}
+      />
+
+      <FormField
+        name="personalInfo.phone"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Phone</FormLabel>
+            <FormControl>
+              <Input placeholder="Please Enter Your Phone Number" {...field} />
+            </FormControl>
+          </FormItem>
+        )}
+      />
+
+      <FormField
+        name="personalInfo.dob"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Date of Birth</FormLabel>
+            <FormControl>
+              <Input
+                type="date"
+                placeholder="Please Enter Your Date of Birth"
+                {...field}
+              />
+            </FormControl>
+          </FormItem>
+        )}
+      />
+
+      <FormField
+        name="personalInfo.profilePicture"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Profile Picture</FormLabel>
+            <FormControl>
+              <Input
+                type="file"
+                accept="image/jpeg, image/png"
+                onChange={(e) => {
+                  if (e.target.files && e.target.files[0]) {
+                    field.onChange(e.target.files[0]);
+                  }
+                }}
+              />
+            </FormControl>
+          </FormItem>
+        )}
+      />
     </div>
   );
 }

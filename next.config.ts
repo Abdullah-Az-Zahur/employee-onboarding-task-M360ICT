ab5d14b1d6a458bf0ae2994b952698c7c41f0759
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2025-08-19 14:08:29 +0600

    fix : (web)  bugs

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  
 };
+
+module.exports = {
+  output: 'standalone', // or 'export' if you're doing static export
+}
 
 export default nextConfig;
